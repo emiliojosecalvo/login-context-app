7289f89f0be48e30f78ba4e1e416302e13606ab6
Emilio Calvo <emiliojosecalvo@gmail.com>
2022-02-11 12:10:35 +0000

    PageContent from class to function

--- src/PageContent.js
+++ src/PageContent.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from './ThemeContext';
 
-export default class PageContent extends Component {
-    static contextType = ThemeContext;
-
-    render() {
-        const { isLightTheme } = this.context;
-        const styles = {
-            backgroundColor: isLightTheme ? '#e7e7e7' : '#000',
-            height: '100vh',
-            width: '100vw'
-        }
-        return (
-            <div style={styles}>{this.props.children}</div>
-        )
+export default function PageContent(props) {
+    const { isLightTheme } = useContext(ThemeContext);
+    const styles = {
+        backgroundColor: isLightTheme ? '#e7e7e7' : '#000',
+        height: '100vh',
+        width: '100vw'
     }
+    return (
+        <div style={styles}>{this.props.children}</div>
+    )
 }
