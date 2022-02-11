c74be3e9f4120fae4f869fff2be88458c8863973
Emilio Calvo <emiliojosecalvo@gmail.com>
2022-02-11 12:07:57 +0000

    ThemeContext from class to function component

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -1,21 +1,13 @@
-import React, { Component, createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const ThemeContext = createContext();
 
-export class ThemeProvider extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { isLightTheme: true }
-        this.toggleTheme = this.toggleTheme.bind(this);
-    }
-    toggleTheme() {
-        this.setState({ isLightTheme: !this.state.isLightTheme });
-    }
-    render() {
-        return (
-            <ThemeContext.Provider value={{ ...this.state, toggleTheme: this.toggleTheme }}>
-                {this.props.children}
-            </ThemeContext.Provider>
-        )
-    }
+export function ThemeProvider(props) {
+    const [isLightTheme, setIsLightTheme] = useState(true);
+    const toggleTheme = () => setIsLightTheme(!isLightTheme)
+    return (
+        <ThemeContext.Provider value={{ isLightTheme, toggleTheme }}>
+            {props.children}
+        </ThemeContext.Provider>
+    )
 }
